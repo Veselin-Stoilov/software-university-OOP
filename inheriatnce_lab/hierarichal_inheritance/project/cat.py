f318a438f7f5febc62a1fcc2a718e0c32f00cb25
Veselin <vesso100@gmail.com>
2021-11-12 09:45:43 +0100

    hierarichal inheratnce added

---
+++ inheriatnce_lab/hierarichal_inheritance/project/cat.py
@@ -0,0 +1,6 @@
+from inheriatnce_lab.single_inheritance.project.animal import Animal
+
+
+class Cat(Animal):
+    def meow(self):
+        return "meowing..."
---
+++ inheriatnce_lab/hierarichal_inheritance/project/dog.py
@@ -0,0 +1,6 @@
+from inheriatnce_lab.single_inheritance.project.animal import Animal
+
+
+class Dog(Animal):
+    def bark(self):
+        return "barking..."
