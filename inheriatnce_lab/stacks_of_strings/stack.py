71aa633df1f6fae5ade09883de6ab1395b8f648a
Veselin <vesso100@gmail.com>
2021-11-12 10:13:26 +0100

    stack of strings, str method fixed

--- inheriatnce_lab/stacks_of_strings/stack.py
+++ inheriatnce_lab/stacks_of_strings/stack.py
@@ -17,4 +17,27 @@ class Stack:
         return True
 
     def __str__(self):
-        return self.data
+        return f"[{', '.join(reversed(self.data))}]"
+
+
+#test zero
+import unittest
+
+
+class StackTests(unittest.TestCase):
+    def test_zero(self):
+        stack = Stack()
+        stack.push("apple")
+        stack.push("carrot")
+        self.assertEqual(str(stack), '[carrot, apple]')
+        self.assertEqual(stack.pop(), 'carrot')
+        self.assertEqual(stack.top(), 'apple')
+        stack.push("cucumber")
+        self.assertEqual(str(stack), '[cucumber, apple]')
+        self.assertEqual(stack.is_empty(), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
