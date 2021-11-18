c411b19bb066be7932e021390d5892d83a995b12
Veselin <vesso100@gmail.com>
2021-11-18 19:30:49 +0100

    restaurant added

---
+++ encapsulation_exercise/restaurant/project/beverage/cold_beverage.py
@@ -0,0 +1,6 @@
+from encapsulation_exercise.restaurant.project.beverage.beverage import Beverage
+
+
+class ColdBeverage(Beverage):
+    def __init__(self, name: str, price: float, milliliters: float):
+        super().__init__(name, price, milliliters)
