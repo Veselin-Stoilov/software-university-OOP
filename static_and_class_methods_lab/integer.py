04eec88129d8c9b4261a5d995730a28dcfafd986
Veselin <vesso100@gmail.com>
2021-11-29 14:30:20 +0100

    integer added

---
+++ static_and_class_methods_lab/integer.py
@@ -0,0 +1,42 @@
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, value):
+        if not isinstance(value, float):
+            return "value is not a float"
+        return cls(int(value))
+
+    @classmethod
+    def from_roman(cls, value):
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        for i, c in enumerate(value):
+            if (i + 1) == len(value) or roman_numerals[c] >= roman_numerals[value[i + 1]]:
+                result += roman_numerals[c]
+            else:
+                result -= roman_numerals[c]
+        return cls(result)
+
+    def __str__(self):
+        return f"Value: {self.value}"
+
+    @classmethod
+    def from_string(cls, value):
+        if not isinstance(value, str):
+            return "wrong type"
+        return cls(int(value))
+
+
+first_num = Integer(10)
+print(first_num.value)
+
+second_num = Integer.from_roman("IV")
+print(second_num.value)
+
+print(Integer.from_float("2.6"))
+print(Integer.from_string(2.6))
+
+
+
