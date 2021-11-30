f56b24d19b7cbb23439ed3daa363ecfeed77a96d
Veselin <vesso100@gmail.com>
2021-11-30 12:01:04 +0100

    photo album added

---
+++ class_and_static_methods_exercise/photo_album.py
@@ -0,0 +1,84 @@
+from math import ceil
+
+
+class PhotoAlbum:
+    photos_per_page = 4
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = [[] for _ in range(self.pages)]
+
+    @classmethod
+    def from_photos_count(cls, photos_count):
+        pages = ceil(photos_count / cls.photos_per_page)
+        return cls(pages)
+
+    def add_photo(self, label: str):
+        for page in self.photos:
+            if len(page) < self.photos_per_page:
+                page.append(label)
+                return f"{label} photo added successfully on page " \
+                       f"{self.photos.index(page) + 1} slot " \
+                       f"{page.index(label) + 1}"
+        return "No more free slots"
+
+    def display(self):
+        result = "-----------" + "\n"
+        for page in self.photos:
+            result += f"{' '.join(['[]' for _ in page])}"
+            result += "\n" + "-----------" + "\n"
+        return result
+
+
+import unittest
+
+
+class TestsPhotoAlbum(unittest.TestCase):
+    def test_init_creates_all_attributes(self):
+        album = PhotoAlbum(2)
+        self.assertEqual(album.pages, 2)
+        self.assertEqual(album.photos, [[], []])
+
+    def test_from_photos_should_create_instace(self):
+        album = PhotoAlbum.from_photos_count(12)
+        self.assertEqual(album.pages, 3)
+        self.assertEqual(album.photos, [[], [], []])
+
+    def test_add_photo_with_no_free_spots(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        result = album.add_photo("prom")
+        self.assertEqual(result, "No more free slots")
+
+    def test_add_photo_with_free_spots(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        self.assertEqual(album.photos, [['baby', 'first grade', 'eight grade', 'party with friends']])
+
+    def test_display_with_one_page(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        result = album.display().strip()
+        self.assertEqual(result, "-----------\n[] [] [] []\n-----------")
+
+    def test_display_with_three_pages(self):
+        album = PhotoAlbum(3)
+        for _ in range(8):
+            album.add_photo("asdf")
+        result = album.display().strip()
+        self.assertEqual(result, "-----------\n[] [] [] []\n-----------\n[] [] [] []\n-----------\n\n-----------")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
