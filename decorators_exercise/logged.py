228f149b5ec437d5dbb4296ab505357144e28eba
Veselin <vesso100@gmail.com>
2021-12-13 16:11:33 +0100

    logged added

---
+++ decorators_exercise/logged.py
@@ -0,0 +1,24 @@
+def logged(func):
+    def wrapper(*args):
+        result = func(*args)
+        info_to_return = f"you called {func.__name__}{args}"
+        info_to_return += "\n" + f"it returned {result}"
+
+        return info_to_return
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+
+
+print(func(4, 4, 4))
+
+
+@logged
+def sum_func(a, b):
+    return a + b
+
+
+print(sum_func(1, 4))
